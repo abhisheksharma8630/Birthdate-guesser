3064c9879f8de8ec87031eebc58eb3acea0e0134
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-06-11 17:17:37 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import { CssBaseline } from '@mui/material'
 import "./index.css";
 import '@fontsource/roboto/500.css';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline/>
     <App />
+    <Analytics/>
   </React.StrictMode>,
 )
